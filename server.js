4021d947fc81152d489a3bfc01ac579717fea07b
Moe Yassine <moeyassine1993@gmail.com>
2021-06-23 14:07:39 -0400

    commented out the shoppingCart routes in server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const errorHandler = require('./lib/error_handler')
 const requestLogger = require('./lib/request_logger')
 
 const purchaseRoutes = require('./app/routes/purchase_routes')
-const shoppingCartRoutes = require('./app/routes/shoppingCart_routes')
+// const shoppingCartRoutes = require('./app/routes/shoppingCart_routes')
 
 // require database configuration logic
 // `db` will be the actual Mongo URI as a string
@@ -60,7 +60,7 @@ app.use(requestLogger)
 // register route files
 app.use(userRoutes)
 app.use(purchaseRoutes)
-app.use(shoppingCartRoutes)
+// app.use(shoppingCartRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
